13ff5bb78b4652dc1eabab338469fa6317f16408
Ivanisimo112 <vane4ka3334@gmail.com>
2021-12-14 15:01:39 +0200

    First HW

---
+++ First/src/com/company/Game.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Game {
+    public void play() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("min: ");
+        int min = scanner.nextInt();
+        System.out.print("max: ");
+        int max = scanner.nextInt();
+        System.out.print("PLAY!\n\n");
+        int hiddenNumber = rand(min, max);
+        int counter = 0;
+        while (true) {
+            int scan = scanner.nextInt();
+            while (scan > max || scan < min) {
+                System.out.println("Введите число в укзаном вами диапазоне");
+                scan = scanner.nextInt();
+            }
+            counter++;
+            if (scan == hiddenNumber) {
+                System.out.println(counter + " попыток потрачено");
+                break;
+            } else if (scan < hiddenNumber) {
+                System.out.println("загаданное чисто больше");
+            } else {
+                System.out.println("загаданное чисто меньше");
+            }
+        }
+
+    }
+
+    private int rand(int min, int max) {
+        return (int) ((Math.random() * ((max - min) + 1)) + min);
+    }
+
+    private int rand() {
+        return rand(0, Integer.MAX_VALUE);
+    }
+}
