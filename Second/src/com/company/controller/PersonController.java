0990e70793984768e0f32537c287ccd23c6a4d50
Ivanisimo112 <vane4ka3334@gmail.com>
2021-12-17 18:58:58 +0200

    Second

--- Second/src/com/company/controller/PersonController.java
+++ Second/src/com/company/controller/PersonController.java
@@ -5,20 +5,32 @@ import com.company.model.Person;
 import com.company.view.PersonView;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class PersonController {
     static String surname;
+    static String regexSurname = "^[A-Z][a-z]{1,20}$";
     static String name;
+    static String regexName = "^[A-Z][a-z]{1,20}$";
     static String patronymic;
+    static String regexPatronymic = "^[A-Z][a-z]{1,20}$";
     static String nickname;
+    static String regexNickname = "^[A-Za-z1-9]{1,10}$";
     static String comment;
+    static String regexComment = "^[A-Za-z1-9]{1,100}$";
     static Group group = null;
     static String telephoneNumberHome;
+    static String regexTelephoneNumberHome = "^[+][1-9]{1,20}$";
     static String telephoneNumber;
+    static String regexTelephoneNumber = "^[+][1-9]{1,20}$";
     static String eMail;
+    static String regexEMail = "^[a-zA-Z0-9]{1,20}+@[a-z]{1,4}\\.[a-z]{1,4}$";
     static String skype;
+    static String regexSkype = "^[a-zA-Z0-9]{1,20}+@[a-z]{1,4}\\.[a-z]{1,4}$";
     static String address;
+    static String regexAddress = "^[a-zA-Z]{3,},[a-zA-Z]{3,},[1-9]{1,3}$";
     static String date;
+    static String regexDate = "^[1-9]{2}.[1-9]{2}.[1-9]{4}$";
     static Scanner scanner = new Scanner(System.in);
     private static PersonView personView = new PersonView();
 
@@ -29,14 +41,34 @@ public class PersonController {
     private static Person addPersonInNotebook() {
         System.out.print("1)Фамилия: ");
         surname = scanner.nextLine();
+        while (!checkRegex(surname, regexSurname)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            surname = scanner.nextLine();
+        }
         System.out.print("2)Имя: ");
         name = scanner.nextLine();
+        while (!checkRegex(name, regexName)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            name = scanner.nextLine();
+        }
         System.out.print("3)Отчество: ");
         patronymic = scanner.nextLine();
+        while (!checkRegex(patronymic, regexPatronymic)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            patronymic = scanner.nextLine();
+        }
         System.out.print("4)Никнейм: ");
         nickname = scanner.nextLine();
+        while (!checkRegex(nickname, regexNickname)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            nickname = scanner.nextLine();
+        }
         System.out.print("5)Комментарий: ");
         comment = scanner.nextLine();
+        while (!checkRegex(comment, regexComment)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            comment = scanner.nextLine();
+        }
         System.out.print("6)Группа(HOME, JOB, FRIENDS, FAMILIAR): ");
         boolean b = true;
         while (b) {
@@ -52,22 +84,50 @@ public class PersonController {
         }
         System.out.print("7)Телефон(домашний): ");
         telephoneNumberHome = scanner.nextLine();
+        while (!checkRegex(telephoneNumberHome, regexTelephoneNumberHome)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            telephoneNumberHome = scanner.nextLine();
+        }
         System.out.print("8)Телефон(мобильный): ");
         telephoneNumber = scanner.nextLine();
+        while (!checkRegex(telephoneNumber,regexTelephoneNumber )) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            telephoneNumber = scanner.nextLine();
+        }
         System.out.print("9)Е-майл: ");
         eMail = scanner.nextLine();
+        while (!checkRegex(eMail, regexEMail)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            eMail = scanner.nextLine();
+        }
         System.out.print("10)Скайп: ");
         skype = scanner.nextLine();
+        while (!checkRegex(skype, regexSkype)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            skype = scanner.nextLine();
+        }
         System.out.print("11)Адрес(город,улица,номер дома): ");
         address = scanner.nextLine();
+        while (!checkRegex(address, regexAddress)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            address = scanner.nextLine();
+        }
         System.out.print("12)Дата: ");
         date = scanner.nextLine();
+        while (!checkRegex(date, regexDate)) {
+            System.out.println("Данные введены не коректно,попробуйте еще раз");
+            date = scanner.nextLine();
+        }
 
         return new Person(surname, name, patronymic, nickname, comment, group, telephoneNumberHome,
                 telephoneNumber, eMail, skype, address, date);
     }
 
-    private static void notebookEntries(){
+    private static boolean checkRegex(String s, String regex) {
+        return Pattern.matches(regex, s);
+    }
+
+    private static void notebookEntries() {
         while (true) {
             System.out.println("Добавим человека в вашу записную книжку");
             personView.addInNotebook(addPersonInNotebook());
